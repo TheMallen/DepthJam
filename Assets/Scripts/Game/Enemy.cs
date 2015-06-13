83aba1f8d910e02fb921fbb14ba91b6799c8cd80
Mathew Allen <mathew.w.allen@gmail.com>
2015-06-13 03:14:03 -0400

    Created boxtacle, created walkrandomzombie... even though it sucks

---
+++ Assets/Scripts/Game/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Enemy : Character 
+{
+	private List<Player> m_Players;
+
+	protected override void Start()
+	{
+		m_Players = Game.a_Instance.a_Players;
+		base.Start();
+	}
+
+	protected void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.collider.gameObject.GetComponent<Player> () != null)
+			OnHitPlayer ();
+
+	}
+
+	protected virtual void OnHitPlayer() {
+	
+	}
+
+}
---
+++ Assets/Scripts/Game/RandomWalkEnemy.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomWalkEnemy : Enemy {
+	
+	const float DEFAULT_SPEED = 0.05f;
+
+	enum Directions {LEFT=0, RIGHT=1, DOWN=2, UP=3};
+
+	private Vector2 m_Facing = Vector2.zero;
+
+	private int m_DebounceCounter = 0;
+
+	// Use this for initialization
+	protected override void Start () 
+	{
+		base.Start();
+		m_MoveSpeed = DEFAULT_SPEED;
+		ChangeDirectionRandomly ();
+		Random.seed = (int)System.DateTime.Now.Ticks;
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) 
+	{
+
+		if (m_DebounceCounter == 0) {
+			ChangeDirectionRandomly ();
+			m_DebounceCounter = 1000;
+			Debug.Log ("Set counter to:" + m_DebounceCounter);
+		} else {
+			Debug.Log ("Subtracting");
+			m_DebounceCounter-=1;
+		}
+
+	}
+	
+	protected override void Update ()
+	{
+		base.Update ();
+		Forward ();
+
+	}
+
+	protected void Forward() {
+		m_Delta = m_Facing;
+	}
+
+	protected void ChangeDirectionRandomly() 
+	{
+		m_Facing = RandomDirection ();
+	}
+
+	protected Vector2 RandomDirection() 
+	{
+		var r = Random.Range (0, 3);
+		switch (r) {
+			case (int)Directions.LEFT:
+				return new Vector2(-1, 0);
+			break;
+			case (int)Directions.RIGHT:
+				return new Vector2(1, 0);
+			break;
+			case (int)Directions.DOWN:
+				return new Vector2(0, 1);
+			break;
+			case (int)Directions.UP:
+				return new Vector2(0, -1);
+			break;
+		}
+		return Vector2.zero;
+
+	}
+
+}
